146f075e28157125fea56b8874ae3132193614f5
siri-boom <siri.n.jain02@gmail.com>
2025-01-11 12:36:51 +0000

    final

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+PRODUCTS = [{
+    'id': 1,
+    'title': 'Pink Crossandra',
+    'price': '2000'
+}, 
+{
+    'id': 2,
+    'title': 'Arabian Jasmine',
+    'price': '2500'
+}, {
+    'id': 3,
+    'title': 'Lucky Bamboo',
+    'price': '2500'
+}, {
+    'id': 3,
+    'title': 'Peace lily',
+    'price': '2500'
+}
+]
 
 @app.route('/')
 def index():
-  return 'Hello world, Flask!'
+  return render_template('home.html', products=PRODUCTS)
+
+@app.route('/products')
+def list_products():
+  return jsonify(PRODUCTS)
 
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
-
-print("hello siri")
